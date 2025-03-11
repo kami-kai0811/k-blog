90e1b47af6e9bcd432a09ad5436fb01b50d5694e
kaito <kamiya0811eng@gmail.com>
2025-03-11 12:02:54 +0900

    huskyの再設定

---
+++ src/components/hello-universe.tsx
@@ -0,0 +1,3 @@
+export function HelloUniverse() {
+  return <h1 className="text-2xl font-bold">Hello Universe</h1>;
+}
--- src/components/hello.tsx
+++
@@ -1,3 +0,0 @@
-export function Hello() {
-  return <h1 className="text-2xl font-bold">Hello world</h1>;
-}

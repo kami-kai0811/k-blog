8f45dc0934b394e472dab0987c490501343bf276
kaito <kamiya0811eng@gmail.com>
2025-03-11 11:27:01 +0900

    eslintの設定

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import { Hello } from '@/components/hello';
+
 export default function Home() {
   return (
     <main>
       <div className="flex justify-center items-center h-screen">
         <div className="flex flex-col items-center">
-          <h1 className="text-2xl font-bold">Hello World</h1>
+          <Hello />
         </div>
       </div>
     </main>
---
+++ src/components/hello.tsx
@@ -0,0 +1,3 @@
+export function Hello() {
+  return <h1 className="text-2xl font-bold">Hello world</h1>;
+}

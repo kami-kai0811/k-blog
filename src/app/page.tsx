00d26cb864b2ba8371715ab0c6cd7be76905263b
kaito <kamiya0811eng@gmail.com>
2025-03-11 13:32:40 +0900

    t3 envの設定

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
 import { HelloUniverse } from "@/components/hello-universe";
+import { env } from "@/env";
 
 export default function Home() {
+  const url = env.DEBUG_URL;
+
   return (
     <main>
       <div className="flex h-screen items-center justify-center">
         <div className="flex flex-col items-center">
           <HelloUniverse />
+          <p className="text-sm text-gray-500">{url}</p>
         </div>
       </div>
     </main>
